2bb6835076da162fca0f1faa18987381a776e52a
Szymon Mikulicz <szymon.mikulicz@posteo.net>
2026-06-13 00:39:23 +0200

    Generation should work now

--- mirror.sh
+++ mirror.sh
@@ -2,16 +2,24 @@
 
 set -e
 
+mkdir -p web git
+cp style.css logo.png web
+
 while read line; do
     srepo="$(echo "$line" | cut -d' ' -f1)"
     reponame="$(echo "$srepo" |  sed 's@.*/@@')"
+    owner="$(echo "$srepo" | sed 's@.*/\(.*\)/.*@\1@')"
     srepodir="git/$reponame"
     wrepodir="web/$reponame"
 
     [ -d "$srepodir" ] || git clone --mirror "$srepo" "$srepodir"
 
+    mkdir -p "$wrepodir"
+    ln -sf ../style.css ../logo.png "$wrepodir"
+    echo "$owner" > "$srepodir/owner"
+    echo "$srepo" > "$srepodir/urls
+    echo ":)" > "$srepodir/description"
     env -C "$wrepodir" stagit "$(readlink -f "$srepodir")"
-    cp style.css logo.png "$wrepodir"
 
     for drepo in $(echo "$line" | cut -d' ' -f2-); do
         git -C "$srepodir" push --mirror "$drepo"
@@ -19,4 +27,3 @@ while read line; do
 done < mirrorlist
 
 stagit-index git/* > web/index.html
-cp style.css logo.png web
